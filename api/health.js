d2aaf2697b3e226053821236443a361ef83d77e9
timharreither-cmyk <tim.harreither@gmail.com>
2026-04-30 19:50:54 +0200

    chan ges

--- api/health.js
+++ api/health.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.json({
     ok: true,
-    hasGithubToken: !!process.env.GH_PAT,
+    hasGithubToken: !!process.env.Github_Token,
     hasBlobToken: !!process.env.BLOB_READ_WRITE_TOKEN,
     node: process.version,
     ts: new Date().toISOString(),
